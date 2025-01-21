76b1d21978b72d383a336c9587500c883a09223c
Nilton Pontes <niltoneapontes@gmail.com>
2025-01-20 21:59:00 -0300

    feat: finishing home

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -9,8 +9,11 @@ export default function Home() {
             <View style={{ width: '100%', height: 52, backgroundColor: '#2B2A33', justifyContent: 'center', padding: 16, borderRadius: 8 }}>
                 <Text style={{ fontWeight: 'regular', fontSize: 20, lineHeight: 20, color: '#FFFFFF' }}>Lista de compras</Text>
             </View>
+            <TouchableOpacity style={{ width: 72, height: 72, borderRadius: 36, backgroundColor: '#FFF59D', position: 'absolute', bottom: 36, right: 36, alignItems: "center", justifyContent: "center"}}>
+                <Text style={{ fontWeight: 'bold', fontSize: 48, color: '#212029', textAlign: 'center', lineHeight: 48 }}>+</Text>
+            </TouchableOpacity>
         </View>
-        <View style={{ width: '100%', minHeight: 300, padding: 16, backgroundColor: '#343342', borderTopLeftRadius: 12, borderTopRightRadius: 12 }}>
+        <View style={{ width: '100%', minHeight: 300, padding: 16, backgroundColor: '#343342', borderTopLeftRadius: 12, borderTopRightRadius: 12, position: 'absolute', bottom: 0, left: 0 }}>
             <Text style={{ position:'absolute', top: 16, right: 16, color: '#FFFFFF', fontSize: 24 }}>x</Text>
             <Text style={{ fontWeight: 'bold', fontSize: 24, color: '#FFFFFF', marginVertical: 24 }}>Nova lista</Text>
             <TextInput style={{ width: '100%', height: 56, borderColor: '#FFFFFF', borderWidth: 1, borderRadius: 8, paddingLeft: 16}} placeholder='Insira o título da lista' placeholderTextColor={'#878787'}></TextInput>
