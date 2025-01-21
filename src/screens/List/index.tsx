bca77772d577570d5646bbf6dd002964a86a766d
Nilton Pontes <niltoneapontes@gmail.com>
2025-01-20 22:30:46 -0300

    feat: creating list page

--- src/screens/List/index.tsx
+++ src/screens/List/index.tsx
@@ -1,16 +1,16 @@
+import { View, Text } from 'react-native'
 import React from 'react'
-import { Text, TextInput, TouchableOpacity, View } from 'react-native'
 import { Title } from '../../components/Title'
 import { Item } from '../../components/Item'
 import { FloatingButton } from '../../components/FloatingButton'
 import BottomSheet from '../../components/BottomSheet'
 
-export function Home() {
+export function List() {
   return (
     <View style={{ flex: 1, width: '100%', backgroundColor: '#212029' }}>
         <View style={{ flex: 1, width: '100%', padding: 16, paddingTop: 100, backgroundColor: '#212029' }}>
-            <Title>Minhas listas</Title>
-            <Item>Lista de compras</Item>
+            <Title>Lista de Compras</Title>
+            <Item>Batata</Item>
             <FloatingButton>+</FloatingButton>
         </View>
         <BottomSheet>Adicionar</BottomSheet>
